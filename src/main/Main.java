ff3c3eb7af604600efc3e387b825ff4d1e0dbe66
oalhambra <o.alhambra@gmail.com>
2017-06-16 17:45:22 +0200

    fix mine placement and add number surrounding

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Tablero tablero=new Tablero(10,20, 100);
+        Tablero tablero=new Tablero(10,10, 70);
         tablero.printTableroTexto();
     }
 }
--- src/main/Tablero.java
+++ src/main/Tablero.java
@@ -5,6 +5,13 @@ import java.util.Random;
 /**
  * Created by espur on 08/06/2017.
  */
+
+
+/**
+ * puesto vacio = -1
+ * mina         = 01
+ * otro numero  = minas alrededor de casilla
+ */
 public class Tablero {
     private int[][] tablero;
     private int alto;
@@ -56,25 +63,54 @@ public class Tablero {
         Random random=new Random(System.currentTimeMillis());
         int minasTablero=0;
         int posx, posy;
+        int contadorFallos=0;
         while (minasTablero<numMinas){
             posx=random.nextInt(alto);
             posy=random.nextInt(ancho);
-            if((posAlto!=posx)&&(posAncho!=posy)){
+            if(!((posAlto==posx)&&(posAncho==posy))){
                 if(tablero[posx][posy]==-1){
-                    if(comprobarAlrededores(posx, posy)){
-                        tablero[posx][posy]=1;
+                    if(comprobarAlrededoresPlus(posx, posy)){
+                        tablero[posx][posy]=0;
                         minasTablero++;
+                        contadorFallos=0;
+                    }
+                    else{
+                        contadorFallos++;
                     }
                 }
             }
+            if(contadorFallos==150){
+                System.out.println("No se ha podido generar el tablero");
+                System.exit(-1);
+            }
         }
+        crearNumeros();
+    }
+    private boolean comprobarAlrededoresPlus(int posx, int posy){
+        tablero[posx][posy]=88;
+        boolean posible=true;
+        int posActualx, posActualy;
+        for (int i=-1;i<=1;i++){
+            for (int j=-1;j<=1;j++){
+                posActualx=posx+i;
+                posActualy=posy+j;
+                if((posActualx>=0&&posActualy>=0)&&(posActualx<alto&&posActualy<ancho)){
+                    posible=posible&&comprobarAlrededores(posActualx,posActualy);
+                }
+            }
+        }
+
+
+
+        tablero[posx][posy]=-1;
+        return posible;
     }
     private boolean comprobarAlrededores(int posx, int posy){
         boolean posible=false;
         int posActualx, posActualy;
         for (int i=-1;i<=1;i++){
             for (int j=-1;j<=1;j++){
-                if(i==0&j==0){
+                if(i==0&&j==0){
                     continue;
                 }
                 posActualx=posx+i;
@@ -88,5 +124,38 @@ public class Tablero {
         }
 
         return posible;
+    }
+    private void crearNumeros(){
+        for(int i=0;i<alto;i++){
+            for (int j=0;j<ancho;j++){
+                if(tablero[i][j]==-1){
+                    minasAlrededores(i,j);
+                }
+            }
+        }
+    }
+    private void minasAlrededores(int posx, int posy){
+        int posActualx, posActualy;
+        int contador=0;
+        for (int i=-1;i<=1;i++){
+            for (int j=-1;j<=1;j++){
+                if(i==0&j==0){
+                    continue;
+                }
+                posActualx=posx+i;
+                posActualy=posy+j;
+                if((posActualx>=0&&posActualy>=0)&&(posActualx<alto&&posActualy<ancho)){
+                    if(tablero[posActualx][posActualy]==0){
+                        contador++;
+                    }
+                }
+            }
+        }
+        if(contador!=0){
+            tablero[posx][posy]=contador;
+        }
+        if(contador==8){
+            System.out.print("hola\n");
+        }
     }
 }
